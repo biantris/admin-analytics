7bd6cba73a8f1b79e341af2df36900c7ff5cf422
biantris <anabeatrizxoliveira@gmail.com>
2023-03-04 23:40:00 -0300

    feat(user): more fixes

---
+++ src/pages/user.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+const User = () => {
+  return (
+    <>
+      <Head>
+        <title>Usuários</title>
+      </Head>
+
+      <h1>Usuários</h1>
+    </>
+  );
+};
+
+export default User;
