8e512fe2cb5595c61200c9b332133576036bfbc7
Beatriz Oliveira <anabeatrizxoliveira@gmail.com>
2023-03-09 18:23:55 -0300

    feat(helth-check): add helth check api (#10)

---
+++ src/service/helthCheckApi/helthCheckRoutes.ts
@@ -0,0 +1,6 @@
+export const GetCpuUsageData =
+  'https://run.mocky.io/v3/b1bc5162-7cf2-4599-b1f5-e3bd58fcf07f';
+export const GetMemoryUsageData =
+  'https://run.mocky.io/v3/d23c3262-967e-4567-b7f6-2fd263748811';
+export const GetClusterStatusInfo =
+  'https://run.mocky.io/v3/cab2791c-7c85-4461-b95c-86bc1a12dc72';
