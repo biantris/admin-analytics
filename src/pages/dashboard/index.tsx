6b41f5b24de42517bfc7e89893f09ea0bb73c05f
biantris <anabeatrizxoliveira@gmail.com>
2023-03-10 13:24:47 -0300

    feat(status): add get cluster status info

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,12 +1,20 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
+import { getClusterStatusInfo } from '../../service/helthCheckApi/helthCheck';
+import { withSSRAuth } from '../../utils/auth/withSSRAuth';
 
 import { ChartBar, dataBar } from '../../components/ChartBar';
 import { ChartPie, dataPie } from '../../components/ChartPie';
 
+import { memo } from 'react';
 import { Nav } from '../../components/Nav';
 import { WrapperDash } from '../../components/WrapperDash';
 
-const Dashboard = () => {
+interface IDashboardProps {
+  status: string;
+}
+
+const Dashboard = ({ status }: IDashboardProps) => {
   return (
     <div>
       <Head>
@@ -24,26 +32,26 @@ const Dashboard = () => {
       <main>
         <WrapperDash>
           <div className="flex flex-col justify-center p-1 h-96 w-80">
-            <h2 className="mt-6 text-center text-1xl font-normal text-gray-900">
+            <h2 className="mt-6 text-center text-1xl font-semibold text-gray-900">
               Consumo de CPU
             </h2>
             <ChartBar data={dataBar} />
           </div>
 
           <div className="flex flex-col justify-center p-1 h-96 w-80">
-            <h2 className="mt-6 text-center text-1xl font-normal text-gray-900">
+            <h2 className="mt-6 text-center text-1xl font-semibold text-gray-900">
               Consumo de Memória
             </h2>
             <ChartPie data={dataPie} />
           </div>
 
           <div className="flex flex-col justify-center p-1 h-96 w-80">
-            <h2 className="mt-6 text-center text-1xl font-normal text-gray-900">
+            <h2 className="mt-6 text-center text-1xl font-semibold text-gray-900">
               Status do Cluster
             </h2>
             <div className="flex flex-col justify-center items-center pt-36 pb-40">
               <span className="bg-green-100 text-green-800 text-xs font-medium w-40 text-center px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">
-                Green
+                {status}
               </span>
             </div>
           </div>
@@ -53,4 +61,16 @@ const Dashboard = () => {
   );
 };
 
-export default Dashboard;
+export const getServerSideProps: GetServerSideProps = withSSRAuth(
+  async (ctx) => {
+    const { status } = await getClusterStatusInfo({ ctx });
+
+    return {
+      props: {
+        status: String(status),
+      },
+    };
+  }
+);
+
+export default memo(Dashboard);
