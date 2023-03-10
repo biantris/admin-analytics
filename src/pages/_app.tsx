07b3312e077fc7d4b0a4c61513ffcf3ebaf28259
biantris <anabeatrizxoliveira@gmail.com>
2023-03-09 21:51:52 -0300

    improve root

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import type { AppProps } from 'next/app';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import 'tailwindcss/tailwind.css';
 import { AuthProvider } from '../context/AuthContext';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
+      <div style={{ position: 'absolute', top: '0', right: '0' }}>
+        <ToastContainer />
+      </div>
       {/* @ts-ignore */}
       <Component {...pageProps} />
     </AuthProvider>
