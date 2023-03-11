37abe26e15fc190ad4681be3cad2d1109e6278ee
biantris <anabeatrizxoliveira@gmail.com>
2023-03-11 19:48:41 -0300

    feat(typo): fix typo

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -53,7 +53,7 @@ const Login = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <Head>
-        <title>Login</title>
+        <title>Login - Admin Analytics</title>
       </Head>
 
       <div className="max-w-sm w-full space-y-8">
