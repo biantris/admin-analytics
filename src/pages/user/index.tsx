446a9711d4f225124120ad34eba1b1ed8ec54d41
biantris <anabeatrizxoliveira@gmail.com>
2023-03-11 02:24:22 -0300

    feat(user-list): add table to list users

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import { GetServerSideProps } from 'next';
 import { Nav } from '../../components/Nav';
 import { WrapperDash } from '../../components/WrapperDash';
@@ -5,10 +6,23 @@ import { withSSRAuth } from '../../utils/auth/withSSRAuth';
 
 import Head from 'next/head';
 import { parseCookies } from 'nookies';
-import { getUser } from '../../service/userApi/user';
+import { Key } from 'react';
+import { getAllUser, getUser } from '../../service/userApi/user';
 import { getInfosDecodedToken } from '../../utils/getInfosDecodedToken';
 
-const User = () => {
+interface IUserProps {
+  userAll: {
+    map(
+      // eslint-disable-next-line no-unused-vars
+      arg0: (userAll: { id: Key | null | undefined }) => JSX.Element
+    ): import('react').ReactNode;
+    name: string;
+    email: string;
+    access_level: string;
+  };
+}
+
+const User = ({ userAll }: IUserProps) => {
   return (
     <div>
       <Head>
@@ -24,11 +38,39 @@ const User = () => {
       </header>
 
       <main>
+        <div className="flex justify-end max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 p-3">
+          <button className="bg-transparent hover:bg-gray-800 text-gray-800 font-semibold hover:text-white py-2 px-4 border border-gray-800 hover:border-transparent rounded">
+            Novo Usuário
+          </button>
+        </div>
         <WrapperDash>
-          <div className="flex flex-col justify-center p-1 h-96 w-80">
-            <h2 className="mt-6 text-center text-1xl font-normal text-gray-900">
+          <div className="flex flex-col justify-center items-center p-1 h-full w-full">
+            <h2 className="text-center text-1xl font-semibold p-3 text-gray-900">
               Usuários
             </h2>
+
+            <div className="overflow-x-auto p-3 pb-8">
+              <table className="table w-full">
+                <thead>
+                  <tr>
+                    <th className="border p-2">Nome</th>
+                    <th className="border p-2">Email</th>
+                    <th className="border p-2">Nível de Acesso</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {userAll.map((userAll) => (
+                    <tr key={userAll.id}>
+                      <td className="border px-4 py-2">{userAll.name}</td>
+                      <td className="border px-4 py-2">{userAll.email}</td>
+                      <td className="border px-4 py-2">
+                        {userAll.access_level}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </WrapperDash>
       </main>
@@ -52,8 +94,12 @@ export const getServerSideProps: GetServerSideProps = withSSRAuth(
       };
     }
 
+    const userAll = await getAllUser({ ctx });
+
+    console.log('USERS', userAll);
+
     return {
-      props: {},
+      props: { userAll },
     };
   }
 );
--- src/service/userApi/userRoutes.ts
+++ src/service/userApi/userRoutes.ts
@@ -1,4 +1,5 @@
 export const AuthUser = 'api/user/auth';
 export const CreateUser = 'api/user/';
+export const GetAllUser = 'api/user/all';
 export const GetUserById = (id: string) => `api/user/${id}`;
 export const GetUserByEmail = (email: string) => `api/user/${email}`;
